6a4fb735eabcf37578501b19a005e69ccd6154d1
Francisco Cejudo Gómez <cejudogomezf@ryanair.com>
2019-06-06 13:24:51 +0200

    Adding some echoes to show information about what we are deploying

--- spring-boot-aws/03-docker-build/docker-build.sh
+++ spring-boot-aws/03-docker-build/docker-build.sh
@@ -5,7 +5,15 @@ source /docker-lib.sh
 
 start_docker
 
+echo "docker has started with version "
+docker --version
+
 $(aws ecr get-login --no-include-email --region eu-west-1)
+
+echo "artifact prefix: $ARTIFACT_NAME"
+echo "to be deployed in registry: $REGISTRY"
+
+ping google.es -c 4
 
 docker build -t $ARTIFACT_NAME -f ./pipeline_code/spring-boot-aws/03-docker-build/Dockerfile ./source_code
 
