5c835809d9eaecff7f329814668c346c92538615
Francisco Cejudo Gómez <cejudogomezf@ryanair.com>
2019-06-07 12:47:42 +0200

    trying to use the Dockerfile in each project

--- spring-boot-aws/03-docker-build/docker-build.sh
+++ spring-boot-aws/03-docker-build/docker-build.sh
@@ -7,7 +7,7 @@ start_docker
 
 $(aws ecr get-login --no-include-email --region eu-west-1)
 
-docker build -t $ARTIFACT_NAME -f ./pipeline_code/spring-boot-aws/03-docker-build/Dockerfile ./source_code
+docker build -t $ARTIFACT_NAME ./source_code
 
 docker tag $ARTIFACT_NAME:latest $REGISTRY/$ARTIFACT_NAME:latest
 
