50bc3275803982b3214e84848501c1a11dd27085
Francisco Cejudo Gómez <cejudogomezf@ryanair.com>
2019-06-18 16:36:37 +0200

    Starting adding level deployment docker into registry

---
+++ angular-aws/02-docker/docker-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+
+source /docker-lib.sh
+
+start_docker
+
+$(aws ecr get-login --no-include-email --region eu-west-1)
+
+ls source_code
+tar -xvvf artifact/$ARTIFACT_NAME*.jar source_code/
+
+ls source_code
+ls source_code/dist
+
+docker build -t $ARTIFACT_NAME source_code
+
+docker tag $ARTIFACT_NAME:latest $REGISTRY/$ARTIFACT_NAME:latest
+
+docker push $REGISTRY/$ARTIFACT_NAME:latest
